042f1dfb9ed0c46fedb82225133096dca28f2c6c
Yannick <91213045+YannickZahinda@users.noreply.github.com>
2022-03-30 18:44:15 +0200

    add design

--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
-import _ from 'lodash';
 import './style.css';
-// function component() {
-//     const element = document.createElement('div');
-  
-//     // Lodash, currently included via a script, is required for this line to work
-//     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-//     element.classList.add('hello');
-  
-//     return element;
-//   }
-  
-// document.body.appendChild(component());
+
+const toDoList = document.querySelector('.todo-list');
+const myTodoTasks = [
+  {
+    Description: 'Learning to code',
+    Completed: false,
+    index: 1,
+  },
+  {
+    Description: 'Learning webpack',
+    Completed: true,
+    index: 2,
+  },
+  {
+    Description: 'Taking weekly quiz',
+    Completed: false,
+    index: 3,
+  },
+  {
+    Description: 'Achieve module one',
+    Completed: true,
+    index: 4,
+  },
+];
+
+const toDoData = () => {
+  for (let task = 0; task < myTodoTasks.length; task += 1) {
+    toDoList.innerHTML += `
+        <li class="task"><span><input type="checkbox" id="checkbox"/></span> &nbsp; &nbsp; ${myTodoTasks[task].Description}<li/>
+        `;
+  }
+};
+
+toDoData();
