5206e095316fed0368ef1c1b2c72f4ece72f66ac
Yannick <91213045+YannickZahinda@users.noreply.github.com>
2022-04-01 16:43:29 +0200

    update html file

--- modules/index.js
+++ modules/index.js
@@ -1,5 +1,3 @@
-import { myTodoTasks } from '../modules/app.js';
-// myTodoTasks.push(new Addtask('Learning to code', false, 1).addTask())
 export default class TodoTaskStorage {
     static getFromStorage = () => JSON.parse(localStorage.getItem('taskInfo'));
     static setToStorage = (myTodoTasks) => localStorage.setItem('taskInfo', JSON.stringify(myTodoTasks));
